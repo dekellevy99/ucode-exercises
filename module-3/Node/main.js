6adff0afda9f5503f495281e81f5941f1d131f64
dekellevy99 <dekelllevy@gmail.com>
2022-08-31 11:09:05 +0300

    node lesson without exercises yet

---
+++ module-3/Node/main.js
@@ -0,0 +1,9 @@
+const enemies = [
+    { e: "Plastic" },
+    { e: "Cigarettes" },
+    { e: "The Man" }
+]
+
+for (const enemy of enemies) {
+    console.log(enemy.e)
+}
---
+++ module-3/Node/shapeApp.js
@@ -0,0 +1,2 @@
+const circleUtils = require("./circleUtils.js");
+console.log(circleUtils)
