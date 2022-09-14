76ca7346cb512eece85c9fd6bcd4b221aca4b721
dekellevy99 <dekelllevy@gmail.com>
2022-09-14 19:57:29 +0300

    a solution of prime factors

--- module-6-Python/Iterators/exercises/prime_factors_generator.py
+++ module-6-Python/Iterators/exercises/prime_factors_generator.py
@@ -1,2 +1,16 @@
+import math
+
 def get_prime_factors_generator(number):
-    
+    i = 2
+    while i * i <= number:
+        if number % i != 0:
+            i += 1
+        else:
+            number = int(number / i)
+            yield i
+    if number > 1:
+        yield number
+
+
+for x in get_prime_factors_generator(100):
+    print(x) 
