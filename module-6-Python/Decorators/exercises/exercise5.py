6282a1ee373599558b419fb17fc0755d074c74dc
dekellevy99 <dekelllevy@gmail.com>
2022-09-16 18:55:43 +0300

    add the extension part

--- module-6-Python/Decorators/exercises/exercise5.py
+++ module-6-Python/Decorators/exercises/exercise5.py
@@ -1,9 +1,10 @@
 def cache_decorator(func):
     results = {}
-    def inner(x):
-        if x not in results:
-            results[x] = func(x)
-        return results[x]
+    def inner(*args, **kwargs):
+        key = (args, frozenset(sorted(kwargs.items())))
+        if key not in results:
+            results[key] = func(*args, **kwargs)
+        return results[key]
     return inner
 
 
@@ -15,4 +16,26 @@ def fibo(n):
     else:
         return fibo(n-1) + fibo(n-2)
 
-print(fibo(9))
+
+@cache_decorator
+def add_stam(*args, **kwargs):
+    print("calculating for = ", args)
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
+
+
+@cache_decorator
+def add(x, *, addition=0, more=0):
+  print("running  add")
+  return x + 1 + addition + more 
+
+
+print("fibo(9) = ", fibo(9))
+print("add(1,2,3, stam=7) = ", add_stam(1, 2, 3, stam=7))
+
+add(1, addition=0, more=0)
+add(1, more=0, addition=0)
+# add(1, more=0)
+# add(1) 
