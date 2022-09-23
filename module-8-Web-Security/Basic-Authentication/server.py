1526f19c6e77a13f734d7c96a31aa0450c693ed2
dekellevy99 <dekelllevy@gmail.com>
2022-09-23 10:02:25 +0300

    upload servre.py

--- module-8-Web-Security/Basic-Authentication/server.py
+++ module-8-Web-Security/Basic-Authentication/server.py
@@ -41,26 +41,26 @@
 #     uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
 
 
-# from fastapi import Depends, FastAPI, HTTPException, status
-# from fastapi.security import HTTPBasic, HTTPBasicCredentials
-# import uvicorn
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import uvicorn
 
-# app = FastAPI()
-# security = HTTPBasic()
+app = FastAPI()
+security = HTTPBasic()
 
-# def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-#     if not (credentials.username == "johnsmith") or not (credentials.password == "swordfish"):
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect email or password",
-#             headers={"WWW-Authenticate": "Basic"},
-#         )
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    if not (credentials.username == "johnsmith") or not (credentials.password == "swordfish"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
     
-#     return credentials.username
+    return credentials.username
 
-# @app.get("/profile")
-# async def main(username: str = Depends(get_current_username)):
-#     return {"username": username}
+@app.get("/profile")
+async def main(username: str = Depends(get_current_username)):
+    return {"username": username}
 
-# if __name__ == "__main__":
-#     uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
